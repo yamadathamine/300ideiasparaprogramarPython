74fcdda12bae73b14a4c1a88277bca73677f7b89
Thamine Yamada <thamineyamada@gmail.com>
2020-06-09 16:45:14 -0300

    adicionando o programa etiqueta

---
+++ 1 Saída Simples/etiqueta.py
@@ -0,0 +1,11 @@
+# Elabore um programa que, apos limpar a tela, 
+# escreve seu nome completo na primeira linha, 
+# seu endereco na segunda, 
+# e o CEP e telefone na terceira.
+import os
+
+os.system('clear') # para OSX 
+#os.system('cls') # para windows
+print("Thamine Yamada")
+print("Endereco: Minha Rua, 567")
+print("CEP: 10000-000 Telefone: (00)12345-6789")
