8af0e05d431a99c6ff49ad3deb7247a2144b9bf0
Thamine Yamada <thamineyamada@gmail.com>
2020-06-09 17:04:16 -0300

    adicionando programa mensagem

---
+++ 1 Saída Simples/mensagem.py
@@ -0,0 +1,13 @@
+# Mensagem - Escreva uma mensagem para uma pessoa de que goste. 
+# Implemente um programa que imprima essa mensagem, e envie-a.
+print("Esta eh uma mensagem para voce!")
+print("Para visualiza-la aperte <enter>")
+teste=raw_input("")
+print("Oi Erich!")
+print("Voce eh o meu melhor amigo!")
+print("Meu companheiro de todas as horas")
+print("Topa tudo e mais um pouco!")
+print("Te amo!")
+print("Ass: Thami")
+
+
