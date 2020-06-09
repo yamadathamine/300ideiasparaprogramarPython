e91e7d6014748dddc453130c4a29e813d4aa8aca
Thamine Yamada <thamineyamada@gmail.com>
2020-06-09 16:55:06 -0300

    adicionando programa frases assassinas

---
+++ 1 Saída Simples/frasesassassinas.py
@@ -0,0 +1,18 @@
+# Frases assassinas
+# Faca um programa que mostre na tela algumas frases assassinas, 
+# que sao aquelas que fazem com muitas ideias sejam perdidas 
+# antes que amadurecam ou seja aprofundadas. 
+# Eis alguns exemplos (bole tambem os seus):
+# "Isto nao vai dar certo" "Voce nunca vai conseguir" "Voce vai se estrepar" 
+# "Nao vai dar em nada" "Esta tudo errado!"
+
+# Pressione enter para ver todas as mensagens
+print("Nao vai dar certo!")
+teste=raw_input("")
+print("Voce nunca vai conseguir!")
+teste=raw_input("")
+print("Voce vai se estrepar!")
+teste=raw_input("")
+print("Nao vai dar em nada!")
+teste=raw_input("")
+print("Esta tudo errado!")
